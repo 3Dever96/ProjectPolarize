3cdb0af1027f0caefd53d267b1006a574bf6d70e
3Dever96 <134976722+3Dever96@users.noreply.github.com>
2026-01-22 11:13:54 -0500

    Player Healthbar implemented

--- Assets/Scripts/Features/Combat/PlayerStats.cs
+++ Assets/Scripts/Features/Combat/PlayerStats.cs
@@ -2,6 +2,29 @@ using UnityEngine;
 
 public class PlayerStats : BaseStats
 {
+    [SerializeField] PlayerHealthbar healthbar;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        healthbar.UpdateHealthbar(currentHp);
+    }
+
+    public override void TakeDamage(int damage)
+    {
+        base.TakeDamage(damage);
+
+        healthbar.UpdateHealthbar(currentHp);
+    }
+
+    public override void RecoverDamage(int recover)
+    {
+        base.RecoverDamage(recover);
+
+        healthbar.UpdateHealthbar(currentHp);
+    }
+
     protected override void Die()
     {
         GameManager.instance.OnGameOver();
